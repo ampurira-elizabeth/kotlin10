6cc1606fdde78e9ffd2a147297e0d0f08813d468
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-02-26 16:43:31 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,37 @@
+fun main(){
+    var string ="akirachix"
+    var char1="a"
+    var char3="i"
+    var char4="r"
+    var result ="a"+"i"+"r"
+    println(result)
+
+    var name="liz"
+    var age="sixty years"
+    var total="Hy my name is"+" "+"liz"+" and i am"+ " "+"sixty years old"
+    println(total)
+length()
+    sentence()
+}
+fun tota(name:String,age:String):String{
+    var total="Hy my name"+" "+"liz"+"and i am"+ " "+"sixty years old"
+    return total
+}
+fun length(){
+    var bus = "Nissan"
+    println(bus.length)
+
+}
+fun number3(){
+
+}
+fun sentence(){
+    var returnSentence = "Thats me"
+    var a = "liz"
+    var sentensi = if (a == "Liz") {
+        "Thats me"
+    } else{
+            "I dont know who that is"
+        }
+            println(returnSentence)
+        }
